1b90f2630dddab216e3dbbf43f630a7ece5f1bc0
jaikrishnaverma <jaikrishnaverma>
2022-10-19 09:27:17 +0530

    dfs

--- src/component/Panel.jsx
+++ src/component/Panel.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import style from './index.module.scss'
 const Panel = () => {
-  let [css, setCss] = useState({magnifier:"",  link: { "fontSize": 20,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":""},h1: { "fontSize": 20,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":"" }, h2: {"fontSize": 18 ,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":"" }, h3: {"fontSize": 15 ,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":"" }, p: {"fontSize": 13,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":"" }, all: {"fontSize": 30,"lineHeight":1.2,"letterSpacing":1 ,"textAlign":"left","color":"","backgroundColor":"" },main:{"zoom":1,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":"" } })
+  let [css, setCss] = useState({magnifier:"",  link: { "fontSize": 20,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":"","filter": "contrast(1)","fontFamily":"sans-serif"},h1: { "fontSize": 20,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":"","filter": "contrast(1)","fontFamily":"sans-serif" }, h2: {"fontSize": 18 ,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":"","filter": "contrast(1)","fontFamily":"sans-serif" }, h3: {"fontSize": 15 ,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":"","filter": "contrast(1)","fontFamily":"sans-serif" }, p: {"fontSize": 13,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":"","filter": "contrast(1)","fontFamily":"sans-serif" }, all: {"fontSize": 30,"lineHeight":1.2,"letterSpacing":1 ,"textAlign":"left","color":"","backgroundColor":"","filter": "contrast(1)","fontFamily":"sans-serif" },main:{"zoom":1,"lineHeight":1.2,"letterSpacing":1,"textAlign":"left","color":"","backgroundColor":"","filter": "contrast(1)","fontFamily":"sans-serif" } })
   let [mode,setMode]=useState("all") 
 
   const Styler = (val,tag=mode) => {
@@ -31,6 +31,22 @@ const Panel = () => {
           <button>Button</button>
           <a href="https://www.youtube.com/" style={{...css.link}} className={`${css.magnifier}`}>YouTube link</a>
         </div>
+        <div className="card">
+          <h1 style={{...css.h1}} className={`${css.magnifier}`}>Hello Brother</h1>
+          <h2 style={{...css.h2}} className={`${css.magnifier}`}>Heading2</h2>
+          <h3 style={{...css.h3}} className={`${css.magnifier}`}>Heading3</h3>
+          <p style={{...css.p}} className={`${css.magnifier}`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quas alias fugiat facilis, harum similique necessitatibus ducimus suscipit odio sunt?</p>
+          <button>Button</button>
+          <a href="https://www.youtube.com/" style={{...css.link}} className={`${css.magnifier}`}>YouTube link</a>
+        </div>
+        <div className="card">
+          <h1 style={{...css.h1}} className={`${css.magnifier}`}>Hello Brother</h1>
+          <h2 style={{...css.h2}} className={`${css.magnifier}`}>Heading2</h2>
+          <h3 style={{...css.h3}} className={`${css.magnifier}`}>Heading3</h3>
+          <p style={{...css.p}} className={`${css.magnifier}`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quas alias fugiat facilis, harum similique necessitatibus ducimus suscipit odio sunt?</p>
+          <button>Button</button>
+          <a href="https://www.youtube.com/" style={{...css.link}} className={`${css.magnifier}`}>YouTube link</a>
+        </div>
 
         <div className="card">
           <h1 style={{...css.h1}} className={`${css.magnifier}`}>Hello Brother</h1>
@@ -46,7 +62,7 @@ const Panel = () => {
 
 
 
-      <div className="row m1 wrap brd5 bgWhite cbox p1" style={{ marginTop: "3%",maxHeight:"845px",width:"39%"}}>
+      <div className="row m1 wrap brd5 bgWhite cbox p1" style={{ marginTop: "3%",maxHeight:"900px",width:"39%"}}>
 
         <div className={`col lbggrey m1 p1 flexAIC w31 brd5`} onClick={()=>setMode('h1')}>
           <span>H1</span>
@@ -73,7 +89,7 @@ const Panel = () => {
             <button className="bin" onClick={() => Styler({ "zoom":css.main.zoom+.1},"main")}><i class="fa fa-angle-up" aria-hidden="true"></i></button>
           </div>
         </div>
-        <div className={`col lbggrey m1 p1 flexAIC ${style.w38} brd5`} onClick={() => Styler({ "fontFamily": "'Times New Roman', serif","fontSize":"20px"})}>
+        <div className={`col lbggrey m1 p1 flexAIC ${style.w38} brd5`} onClick={() => Styler({ "fontFamily": "'Times New Roman', serif","fontSize":20})}>
           <i className="mt fa fa-font" aria-hidden="true"></i>
           <span>Readable Font</span>
         </div>
@@ -146,6 +162,22 @@ const Panel = () => {
         </div>
 
         <div className={`col lbggrey m1 p1 flexAIC brd5`} style={{width:"96%"}}>
+          <p>Adjust Background Colors</p>
+          <div className="row flexAIC">
+          <div className="coldiv" style={{backgroundColor:"#006fb4"}} onClick={() => Styler({backgroundColor:"#006fb4"})}></div>
+            <div className="coldiv" style={{backgroundColor:"#89399c"}} onClick={() => Styler({backgroundColor:"#89399c"})}></div>
+            <div className="coldiv" style={{backgroundColor:"#e00034"}} onClick={() => Styler({backgroundColor:"#e00034"})}></div>
+            <div className="coldiv" style={{backgroundColor:"#e26721"}} onClick={() => Styler({backgroundColor:"#e26721"})}></div>
+            <div className="coldiv" style={{backgroundColor:"#009f9f"}} onClick={() => Styler({backgroundColor:"#009f9f"})}></div>
+            <div className="coldiv" style={{backgroundColor:"#2b8230"}} onClick={() => Styler({backgroundColor:"#2b8230"})}></div>
+            <div className="coldiv" style={{backgroundColor:"white"}} onClick={() => Styler({backgroundColor:"white"})}></div>
+            <div className="coldiv" style={{backgroundColor:"black"}} onClick={() => Styler({backgroundColor:"black"})}></div>
+          </div>
+          <p>Cancel</p>
+        </div>
+
+
+        <div className={`col lbggrey m1 p1 flexAIC brd5`} style={{width:"96%"}}>
           <p>Adjust Title Colors</p>
           <div className="row flexAIC">
           <div className="coldiv" style={{backgroundColor:"#006fb4"}} onClick={() => Styler({backgroundColor:"#006fb4"},"h1")}></div>
@@ -160,19 +192,21 @@ const Panel = () => {
           <p>Cancel</p>
         </div>
 
-        <div className={`col lbggrey m1 p1 flexAIC brd5`} style={{width:"96%"}}>
-          <p>Adjust Background Colors</p>
-          <div className="row flexAIC">
-          <div className="coldiv" style={{backgroundColor:"#006fb4"}} onClick={() => Styler({backgroundColor:"#006fb4"})}></div>
-            <div className="coldiv" style={{backgroundColor:"#89399c"}} onClick={() => Styler({backgroundColor:"#89399c"})}></div>
-            <div className="coldiv" style={{backgroundColor:"#e00034"}} onClick={() => Styler({backgroundColor:"#e00034"})}></div>
-            <div className="coldiv" style={{backgroundColor:"#e26721"}} onClick={() => Styler({backgroundColor:"#e26721"})}></div>
-            <div className="coldiv" style={{backgroundColor:"#009f9f"}} onClick={() => Styler({backgroundColor:"#009f9f"})}></div>
-            <div className="coldiv" style={{backgroundColor:"#2b8230"}} onClick={() => Styler({backgroundColor:"#2b8230"})}></div>
-            <div className="coldiv" style={{backgroundColor:"white"}} onClick={() => Styler({backgroundColor:"white"})}></div>
-            <div className="coldiv" style={{backgroundColor:"black"}} onClick={() => Styler({backgroundColor:"black"})}></div>
-          </div>
-          <p>Cancel</p>
+       
+
+
+        <div className={`col lbggrey m1 p1 flexAIC w31 brd5`}    onClick={() => Styler({filter: "contrast(50%)"})}>
+        <i class="m1 fa fa-moon-o" aria-hidden="true"></i>
+          <span>Dark Contrast</span>
+        </div>
+
+        <div className="w31 col lbggrey m1 p1 flexAIC brd5"   onClick={() => Styler({filter: "contrast(1.3)"})}>
+        <i class="m1 fa fa-sun-o" aria-hidden="true"></i>
+          <span>Light Contrast</span>
+        </div>
+        <div className="w31 col lbggrey m1 p1 flexAIC brd5"  onClick={() => Styler({filter: "contrast(1.8)"})}>
+        <i class="m1 fa fa-adjust" aria-hidden="true"></i>
+          <span>High Contrast</span>
         </div>
       </div>
 
